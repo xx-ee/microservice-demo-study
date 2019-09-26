61f2813d64dee8093615d4e93e7f4e0700d14b31
dong <dxiesjz@163.com>
2019-09-26 08:54:13 +0800

    SpringCloud Bus配置自动刷新

--- microservice-config-client3355/src/main/java/com/microservice/controller/TestController.java
+++ microservice-config-client3355/src/main/java/com/microservice/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.microservice.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class TestController {
     @Value("${spring.application.name}")
     private String applicationname;
@@ -22,6 +24,6 @@ public class TestController {
         System.out.println("applicationName:"+applicationname);
         System.out.println("eurekaServers:"+eurekaServsers);
         System.out.println("port:"+port);
-        return "applicationName:"+applicationname+"\t\n"+"eurekaServers:"+eurekaServsers+"\t\n"+"port:"+"\t"+port;
+        return "应用名:"+applicationname+"\r\n"+"\r\n注册中心列表:"+eurekaServsers+"\r\n"+"\r\n端口号:"+"\t"+port;
     }
 }
